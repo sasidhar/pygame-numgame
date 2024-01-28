fc89815b1562e088cf66dee518840a5a19ced866
Sasidhar Vanga <sasidhar@sasidhar.com>
2024-01-28 08:46:21 -0800

    Playable Game

--- board.py
+++ board.py
@@ -1,10 +1,31 @@
 import pygame
 from random import choice
 
-from constants import BUTTON_WIDTH, BUTTON_COLOR, BUTTON_HEIGHT, GAME_GRID_SIZE, BUTTON_PADDING, BUTTON_TEXT_COLOR, BUTTON_LAST_COLOR
+from constants import (
+    BUTTON_WIDTH,
+    BUTTON_COLOR,
+    BUTTON_HEIGHT,
+    GAME_GRID_SIZE,
+    BUTTON_PADDING,
+    BUTTON_TEXT_COLOR,
+    BUTTON_LAST_COLOR,
+    GAME_BACKGROUND,
+    GAME_HEIGHT,
+    GAME_WIDTH,
+    GAME_DONE_HEIGHT,
+    GAME_DONE_PADDING,
+)
+
+
+def checkGameCompletion():
+    for index, item in enumerate(GAME_BOARD):
+        if index != item:
+            return False
+    return True
+
 
 def generateBoardPosition():
-    board = list(range(0, GAME_GRID_SIZE* GAME_GRID_SIZE))
+    board = list(range(0, GAME_GRID_SIZE * GAME_GRID_SIZE))
     randomized_board = []
     while len(board) > 0:
         board_choice = choice(board)
@@ -12,28 +33,66 @@ def generateBoardPosition():
         board.remove(board_choice)
     return randomized_board
 
+
+GAME_BOARD = generateBoardPosition()
+
+
+def updateBoard(displaysurface, displace=0):
+    blank_value = GAME_GRID_SIZE * GAME_GRID_SIZE - 1
+    blank_index = GAME_BOARD.index(blank_value)
+    if abs(displace) == 1:
+        if displace == -1 and blank_index % GAME_GRID_SIZE == 0:
+            return
+        if displace == 1 and blank_index % GAME_GRID_SIZE == GAME_GRID_SIZE - 1:
+            return
+
+    swap_index = blank_index + displace
+    if swap_index >= 0 and swap_index <= blank_value:
+        temp = GAME_BOARD[swap_index]
+        GAME_BOARD[swap_index] = GAME_BOARD[blank_index]
+        GAME_BOARD[blank_index] = temp
+        drawBoard(displaysurface)
+
+    is_game_completed = checkGameCompletion()
+    if is_game_completed:
+        drawGameCompletion(displaysurface)
+
+
+def drawGameCompletion(displaysurface):
+    drawButton(
+        displaysurface,
+        0,
+        (GAME_HEIGHT / 2) - GAME_DONE_HEIGHT,
+        GAME_WIDTH - 2 * GAME_DONE_PADDING,
+        GAME_DONE_HEIGHT,
+        "GAME DONE",
+        False,
+    )
+
+
 def drawBoard(displaysurface):
-    board = generateBoardPosition()
-    print(board)
+    displaysurface.fill(GAME_BACKGROUND)
     for i in range(0, GAME_GRID_SIZE):
         for j in range(0, GAME_GRID_SIZE):
             b_width = BUTTON_WIDTH
             b_height = BUTTON_HEIGHT
             b_x = j * BUTTON_WIDTH + (j + 1) * BUTTON_PADDING
             b_y = i * BUTTON_HEIGHT + (i + 1) * BUTTON_PADDING
-            b_text = board[(i * GAME_GRID_SIZE + j)] + 1
+            b_text = GAME_BOARD[(i * GAME_GRID_SIZE + j)] + 1
             is_last = b_text == GAME_GRID_SIZE * GAME_GRID_SIZE
             drawButton(displaysurface, b_x, b_y, b_width, b_height, b_text, is_last)
     pygame.display.update()
 
 
 def drawButton(displaysurface, x, y, width, height, button_text, is_last):
+    if is_last:
+        return
+
     button_background_color = BUTTON_COLOR
     if is_last:
         button_background_color = BUTTON_LAST_COLOR
-    pygame.draw.rect(displaysurface, button_background_color, (x,y,width, height))
-    font = pygame.font.SysFont('Arial', 25)
+    pygame.draw.rect(displaysurface, button_background_color, (x, y, width, height))
+    font = pygame.font.SysFont("Arial", 25)
     text = font.render(str(button_text), True, BUTTON_TEXT_COLOR)
-    text_rect = text.get_rect(center=(x + width/2, y + height/2))
+    text_rect = text.get_rect(center=(x + width / 2, y + height / 2))
     displaysurface.blit(text, text_rect)
-    
--- numgame.py
+++ numgame.py
@@ -1,14 +1,13 @@
 import pygame
 from pygame.locals import *
 
-from constants import GAME_HEIGHT, GAME_WIDTH, GAME_BACKGROUND
-from board import drawBoard
+from constants import GAME_HEIGHT, GAME_WIDTH, GAME_GRID_SIZE
+from board import drawBoard, updateBoard
 
 pygame.init()
 
 
 displaysurface = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
-displaysurface.fill(GAME_BACKGROUND)
 pygame.display.set_caption("NumGame 1.0")
 
 run = True
@@ -16,25 +15,22 @@ run = True
 drawBoard(displaysurface)
 
 while run:
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-            run =False
+            run = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                print("Left")
+                updateBoard(displaysurface, +1)
 
             if event.key == pygame.K_RIGHT:
-                print("Right")
+                updateBoard(displaysurface, -1)
 
             if event.key == pygame.K_UP:
-                print("Up")
+                updateBoard(displaysurface, GAME_GRID_SIZE)
 
             if event.key == pygame.K_DOWN:
-                print("Down")
-
+                updateBoard(displaysurface, -1 * GAME_GRID_SIZE)
 
 
 pygame.quit()
